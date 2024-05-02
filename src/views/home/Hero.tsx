7f572c82048d18d92b97c067a30d3a403a765bcf
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-05-02 20:26:26 +0100

    fix(component): hero section tweak
    re-edited the hero section, changed up some styles, and reconfigured tailwind.config.ts

--- src/views/home/Hero.tsx
+++ src/views/home/Hero.tsx
@@ -7,10 +7,13 @@ import users from "@/assets/img/using-devices.svg";
 
 export default function Hero() {
   return (
-    <section className="block sm:flex pt-[3rem] md:pt-[2rem] px-[8vw] sm:max-md:px-[5vw] lg:px-[10vw] gap-[1rem]">
+    <section className="block sm:flex pt-[3rem] pb-[2rem] md:pt-[3.5rem] px-[8vw] sm:max-md:px-[5vw] lg:px-[10vw] gap-[1rem] hero_grad">
       <div className="w-[100%] sm:w-[40dvw] mt-[2rem] gap-[3rem]">
         <h1 className="font-[600] md:text-[3.15rem] text-[2.5rem] mb-4 pointer-events-none">
-          Making online messaging quick and seamless.
+          Making online messaging quick and seamless
+          <span className="text-[#1ca7ec] font-extrabold">
+            .
+          </span>
         </h1>
         <h2 className="font-[300] text-[13px] w-[100%] sm:w-[80%] mb-6 pointer-events-none">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
@@ -19,7 +22,8 @@ export default function Hero() {
         <div className="flex mb-6 items-center flex-wrap gap-6">
           <Link href={"/"}>
             <Button
-              className={`relative w-[140px] md:w-[120px] h-[40px] border border-black !rounded-[4px] hover:text-white hover:bg-black hover:before:block hover:after:block  
+              className={`relative w-[140px] md:w-[120px] h-[40px] border border-black !rounded-[4px] hover:text-white hover:bg-black hover:before:block hover:after:block 
+
                 before:hidden before:content-[' '] before:absolute before:top-[-6px] before:left-[-6px] before:w-[120px] before:h-[40px] before:border-t before:border-s before:rounded-[2px] before:border-t-[#1ca7ec] before:border-s-[#1ca7ec] 
                 
                  after:hidden after:content-[' '] after:absolute after:bottom-[-6px] after:right-[-6px] after:w-[120px] after:h-[40px] after:border-b after:border-e after:rounded-[2px] after:border-b-[#88d6fd] after:border-e-[#88d6fd] 
